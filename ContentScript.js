d85576b3d98ae2ef6e2e1b4869a96c6748587fe3
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-11-11 20:39:21 +0000

    general improvements

--- ContentScript.js
+++ ContentScript.js
@@ -2,8 +2,8 @@ Main();
 
 chrome.runtime.onMessage.addListener(Main);
 
-function Main () {
-    ["span", "title", "a", "p", "h1","h2","h3","h4", "h5", "h6"].forEach(tag => Typoifier(tag));
+function Main() {
+    ["span", "title", "a", "p", "h1", "h2", "h3", "h4", "h5", "h6", "td"].forEach(tag => Typoifier(tag));
 }
 
 function getRandom(max) {
@@ -15,10 +15,10 @@ function AtPos(str, position, newStr) {
 }
 
 function TypoSTR(str) {
-    if(str.length === 0) return
+    if (str.length === 0) return
     let words = str.split(" ");
     words.forEach((word, index) => {
-        if(word.length === 0) return
+        if (word.length === 0) return
         if (getRandom(2)) words[index] = Typo(word);
     })
     return words.join(" ");
@@ -28,14 +28,15 @@ function Typo(word) {
     let index = getRandom(word.length);
     let letter = word[index];
     let newString = AtPos(word, index, letter);
-    if(getRandom(2)) newString = AtPos(newString, index, letter);
+    if (getRandom(2)) newString = AtPos(newString, index, letter);
     return newString;
 }
 
 function Typoifier(tagName) {
     document.querySelectorAll(tagName).forEach(e => {
-	if(e.innerText === "") return
-	if(e.childElementCount !== 0) return
-        e.innerText = TypoSTR(e.innerText);
+        e.childNodes.forEach(node => {
+            if (node.data === "" || node.data === undefined) return
+            node.data = TypoSTR(node.data);
+        })
     });
 };
